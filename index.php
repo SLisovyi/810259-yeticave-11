96321f36d987ab8455b22dac03f617082a3db5e2
Sergii Lisovyi <slisoviy@gmail.com>
2019-10-30 18:21:42 -0600

    убрал второй if

--- index.php
+++ index.php
@@ -32,26 +32,25 @@ $user_name = 'Sergii'; // укажите здесь ваше имя
 
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
 
-        <!-- пользователь залогинин -->
-        <?php if ($is_auth === 1): ?>
-        <div class="user-menu__logged">
-            <p><?=$user_name;?></p>
-            <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-            <a class="user-menu__logout" href="#">Выход</a>
-        </div>
-        <?php endif; ?>
+            <!-- пользователь залогинин -->
+            <?php if ($is_auth === 1): ?>
+            <div class="user-menu__logged">
+                <p><?=$user_name;?></p>
+                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+                <a class="user-menu__logout" href="#">Выход</a>
+            </div>
 
-        <!-- пользователя нет -->
-        <?php if ($is_auth === 0): ?>
-        <ul class="user-menu__list">
-            <li class="user-menu__item">
-                <a href="#">Регистрация</a>
-            </li>
-            <li class="user-menu__item">
-                <a href="#">Вход</a>
-            </li>
-        </ul>
-        <?php endif; ?>
+            <!-- пользователя нет -->
+            <?php else: ?>
+            <ul class="user-menu__list">
+                <li class="user-menu__item">
+                    <a href="#">Регистрация</a>
+                </li>
+                <li class="user-menu__item">
+                    <a href="#">Вход</a>
+                </li>
+            </ul>
+            <?php endif; ?>
 
         </nav>
     </div>

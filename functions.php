71c96cb614bd28b9e1b714e86456531ee637880e
Sergii Lisovyi <slisoviy@gmail.com>
2019-11-09 08:58:03 -0700

    разбиение на модули-правки 3

--- functions.php
+++ functions.php
@@ -15,20 +15,3 @@ function esc($text) {
     
     return $text;   
 }
-
-function include_template($name, $data) {
-    $name = 'templates/' . $name;
-    $result = '';
-
-    if (!file_exists($name)) {
-        return $result;
-    }
-
-    ob_start();
-    extract($data);
-    require $name;
-
-    $result = ob_get_clean();
-
-    return $result;
-}

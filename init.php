3a3369a8638d41d7985bb87a06a5797cb91c4bab
Sergii Lisovyi <slisoviy@gmail.com>
2019-11-23 12:58:50 -0700

    Читаем и показываем записи из MySQL - правки 4

--- init.php
+++ init.php
@@ -3,13 +3,9 @@
 require_once 'config/db.php';
 
 $link = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database']);
-mysqli_set_charset($link, "utf8");
 
 if (!$link) {
     $error = mysqli_connect_error();
-    $content = include_template('error.php', ['error' => $error]);
-    die('Ошибка соединения: ' . mysql_error());
-} else {
-    echo 'Успешно соединились';
-    mysql_close($link);
+    die('Ошибка');
 }
+mysqli_set_charset($link, "utf8");
